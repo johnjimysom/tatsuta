b423ff625bdb2ad5b2138d655dcb0b98175f8f81
John-Jimi Som <johnjimy.som@student.csulb.edu>
2021-06-10 15:04:22 -0700

    updated 'csv_read_record_search.cpp'

--- csv_read_record_search.cpp
+++ csv_read_record_search.cpp
@@ -24,10 +24,11 @@ int main() {
     string quantities;
     string notes;
     string date;
-   // string vendor;
-   // string description;
-    
-    
+    // string vendor;
+    // string description;
+
+
+
     while (ip.good()) { //or EOF: end of file
 
         getline(ip, barcode, ',');
@@ -40,6 +41,36 @@ int main() {
 
        //input the hex from barcode
        //...
+
+        cout << "string: " << barcode << endl; //debug
+        cout << "hexval: "; //debug to test
+        {
+            string sReturn = "";
+            for (unsigned int i = 0; i < barcode.length(); ++i)
+            {
+                switch (toupper(barcode[i]))
+                {
+                case '0': sReturn.append("0000"); break;
+                case '1': sReturn.append("0001"); break;
+                case '2': sReturn.append("0010"); break;
+                case '3': sReturn.append("0011"); break;
+                case '4': sReturn.append("0100"); break;
+                case '5': sReturn.append("0101"); break;
+                case '6': sReturn.append("0110"); break;
+                case '7': sReturn.append("0111"); break;
+                case '8': sReturn.append("1000"); break;
+                case '9': sReturn.append("1001"); break;
+                case 'A': sReturn.append("1010"); break;
+                case 'B': sReturn.append("1011"); break;
+                case 'C': sReturn.append("1100"); break;
+                case 'D': sReturn.append("1101"); break;
+                case 'E': sReturn.append("1110"); break;
+                case 'F': sReturn.append("1111"); break;
+                }
+            }
+            //return sReturn;
+        }
+
 
         std::cout << "Barcode: " << barcode << '\n';
         std::cout << "Quantities: " << quantities << '\n';
